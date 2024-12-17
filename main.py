96e027d89f8fca380b6593b44bdb04c90340d222
Svetlana <skyfall12@mail.ru>
2024-12-17 15:31:29 +0300

    module_9_3

---
+++ main.py
@@ -0,0 +1,9 @@
+first = ['Strings', 'Student', 'Computers']
+
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
+second_result = (len(first[index]) == len(second[index]) for index in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
